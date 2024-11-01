761dc15e91c0f9b8eb4731961ef0c76591722723
Anandya Almas <byanmalang123@gmail.com>
2024-11-01 07:51:30 +0700

    percobaan 4 nomor 3

--- SearchNilai03.java
+++ SearchNilai03.java
@@ -4,33 +4,35 @@ public class SearchNilai03 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+
+        System.out.print("Masukkan banyaknya nilai yang akan diinput");
         int jumlahNilai = sc.nextInt();
-        int [] arrNilai = new int [jumlahNilai];
+        int[] arrNilai = new int[jumlahNilai];
 
         for (int i = 0; i < jumlahNilai; i++) {
-            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1)+ ": ");
             arrNilai[i] = sc.nextInt();
-
         }
-        System.out.print("Masukkan nilai mahasiswa yang ingin dicari: ");
+
+        System.out.print("Masukkan nilai yang ingin dicari: ");
         int key = sc.nextInt();
+
         int hasil = -1;
         for (int i = 0; i < arrNilai.length; i++) {
             if (key == arrNilai[i]) {
                 hasil = i + 1;
                 break;
-
                 
             }
         }
         System.out.println();
         if (hasil != -1) {
-            System.out.println("Nilai" + key + " ketemu, merupakan nilai mahasiswa ke-" + hasil);
+            System.out.println("Nilai " + key + " Ketemu di indeks ke-" + hasil);
         } else {
-            System.out.println("Nilai" + key + " tidak ditemukan dalam array.");
-        }
-        sc.close();
-        }
+            System.out.println("Nilai yang dicari tidak ditemukan");
 
+        }
+            
+        }
     }
+
